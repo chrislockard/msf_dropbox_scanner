dccc54b267947a32e6a7530412fc3fe6eefbbd50
Chris Lockard <chris.lockard@owasp.org>
2014-12-03 15:35:16 -0500

    Fixed broken module.

--- dropbox.rb
+++ dropbox.rb
@@ -7,22 +7,24 @@ require 'msf/core'
 
 class Metasploit3 < Msf::Auxiliary
 
+  include Msf::Exploit::Remote::Tcp
   include Msf::Auxiliary::Scanner
   include Msf::Auxiliary::Report
 
   def initialize
     super(
       'Name'        => 'Dropbox scanner',
-      'Description' => 'This module scans for dropbox listeners on default TCP and UDP ports', 
+      'Version'     => '1.1',
+      'Description' => 'This module scans for dropbox listeners on default TCP port 17500', 
       'Author'      => 'Dagorim - penetrate.io',
       'License'     => MSF_LICENSE
     )
-
+    
     register_options(
-      [
-        Opt::RPORT(17500),
-        OptInt.new('TIMEOUT', [true, 'Timeout for Dropbox probe', 30])
-      ], self.class)
+    [
+      Opt::RPORT(17500),
+      OptInt.new('TIMEOUT', [true, 'Timeout for Dropbox probe', 30])
+    ], self.class)
   end
 
   def to
@@ -34,15 +36,16 @@ class Metasploit3 < Msf::Auxiliary
     begin
       ::Timeout.timeout(to) do
         res = connect
-        banner_sanitized = Rex::Text.to_hex_ascii(banner.to_s)
-        print_status("#{ip}:#{rport} DROPBOX#{banner_sanitized}")
-        report_service(:host => rhost, :port => rport, :name => "Dropbox", :info => banner_sanitized)
-    end
+        print_status("DROPBOX Service Listening: #{ip}:#{rport}")
+        report_service(:host => rhost, :port => rport, :name => "Dropbox")
+      end
+    
     rescue ::Rex::ConnectionError
     rescue Timeout::Error
         print_error("#{target_host}:#{rport}, Server timed out after #{to} seconds.  Skipping.")
     rescue ::Exception => e
         print_error("#{e},#{e.backtrace}")
     end
+
   end
 end
